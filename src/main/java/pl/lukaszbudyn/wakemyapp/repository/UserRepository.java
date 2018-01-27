ec92f1eea61fd547e84403a84523afe838d62538
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-27 18:22:55 +0100

    UserRepository

---
+++ src/main/java/pl/lukaszbudyn/wakemyapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.lukaszbudyn.wakemyapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.lukaszbudyn.wakemyapp.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
