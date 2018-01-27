d26d548dcf77adbfcea56b98e73ddcbb0893f892
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-01-27 14:16:37 +0100

    WebPinger class to ping selected webs with GET request

--- src/main/java/pl/lukaszbudyn/wakemyapp/controller/HomeController.java
+++ src/main/java/pl/lukaszbudyn/wakemyapp/controller/HomeController.java
@@ -3,11 +3,18 @@ package pl.lukaszbudyn.wakemyapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import pl.lukaszbudyn.wakemyapp.app.WebPinger;
+
 @Controller("/")
 public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
+		try {
+			WebPinger.pingWebsites();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "index";
 	}
 }
